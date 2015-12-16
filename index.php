aaa0a3d7579a083eab9e2f393e62eb9d399231f9
gideaoms <gideaoms@gmail.com>
2015-12-16 15:44:09 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,7 @@
 class DestacarPesquisa {
 
     private $busca = "simplesmente é XVI editoração";
-    private $conteudo = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e "
-    . "vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou "
-    . "para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a "
-    . "editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou "
-    . "decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração "
-    . "eletrônica como Aldus PageMaker.";
+    private $conteudo = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.";
     private $countConteudo;
     private $quantidadeTexto = 20;
     private $numeroIniciador;
@@ -37,6 +32,10 @@ class DestacarPesquisa {
             endforeach;
         endforeach;
         return implode(' ', $this->conteudo);
+    }
+
+    private function pegarParteConteudo() {
+        return array_slice($this->conteudo, $this->numeroIniciador, $this->quantidadeTexto);
     }
 
     private function setNumeroIniciador($key) {
